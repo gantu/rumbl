88338b730265130149478ad14f6f49e717e90537
gantu <mipasov@gmail.com>
2021-08-04 14:18:20 +0600

    user is authenticated in socket

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -10,16 +10,17 @@ defmodule RumblWeb.Auth do
         user_id = get_session(conn, :user_id)
 
         cond do
-            conn.assigns[:current_user] -> conn
-            user = user_id && Rumbl.Accounts.get_user(user_id) -> assign(conn, :current_user, user)
-            true -> assign(conn, :current_user, nil)
-        end
+          user = conn.assigns[:current_user] -> put_current_user(conn, user)
+	    
+          user = user_id && Rumbl.Accounts.get_user(user_id) -> put_current_user(conn, user)
 
+	  true -> assign(conn, :current_user, nil)
+        end
     end
 
     def login(conn, user) do
         conn
-        |> assign(:current_user, user)
+        |> put_current_user(user)
         |> put_session(:user_id, user.id)
         |> configure_session(renew: true)
     end
@@ -38,4 +39,13 @@ defmodule RumblWeb.Auth do
             |> halt()
         end
     end
+
+    defp put_current_user(conn, user) do
+      token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+      conn
+      |> assign(:current_user, user)
+      |> assign(:user_token, token)
+    end
+    
 end
